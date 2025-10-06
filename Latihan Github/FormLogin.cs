48ed79dfa773063994816877a82ab87e0c3bbad5
ACER <ACER@LAPTOP-9583MKE1>
2025-10-06 09:39:02 +0800

    Add project files.

---
+++ Latihan Github/FormLogin.cs
@@ -0,0 +1,15 @@
+namespace Latihan_Github
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
